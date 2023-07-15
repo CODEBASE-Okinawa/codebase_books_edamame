85bd47624728f72876b44310cc348d2b2b7e8d4a
koikeasuka <akoike@big-gate.co.jp>
2023-07-15 18:32:36 +0900

    adminのindexのルーティングの不要なものを削除しました。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :admin do
-    resources :books, only: [:index, :new, :create]
+    resources :books, only: [:index]
   end
 end
