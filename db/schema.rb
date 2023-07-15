bb07a46f868965930b2de2d0fcdc9c4fc4acccdf
HY <givhvyvuc@gmail.com>
2023-07-15 15:05:50 +0900

    Bookテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_055501) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_060408) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "mail"
     t.string "name"
