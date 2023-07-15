2b8db5d34a6fc6462b7034b950848e51d99df02f
aayaa2610 <smhi26102511@gmail.com>
2023-07-15 18:46:48 +0900

    books action作成しました

--- app/controllers/reservations_controller.rb
+++
@@ -1,8 +0,0 @@
-class ReservationsController < ApplicationController
-    def index
-    end
-
-    def show
-    end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :reservations, only: [:index, :show]
+  #resources :reservations, only: [:index, :show]
+
+  resources :books, only: [:show]
 
 end
