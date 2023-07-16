1c769ec753af917c55bf435a198ec7e2fa417933
koikeasuka <akoike@big-gate.co.jp>
2023-07-16 11:28:48 +0900

    admin/books/create action作成（画像の保存、及び画像保存先のパスとタイトルのデータベース登録。また、admin/books/newのビューファイル作成

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,4 +1,31 @@
 class Admin::BooksController < ApplicationController  
   def index
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    title = params[:book][:title]
+    image = params[:book][:image]
+    image_name = image.original_filename
+    image_path = "public/images/#{image_name}"
+    
+    if image #画像がある場合は画像をpublicに保存、パスをデータベースに保存
+      File.binwrite(image_path, image.read)
+      @book = Book.new(
+        title: title,
+        image_path: image_path
+      ) 
+    else #画像がない場合はデフォルト画像のパスをデータベースに保存
+      @book = Book.new(
+        title: title,
+        image_path: "public/images/noImage.jpg"
+      )
+    end
+    @book.save
+
+    redirect_to action: :index
+  end
 end
