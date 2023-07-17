57aab37cc92b5aef86718f231ea8a0a7460504d6
aayaa2610 <smhi26102511@gmail.com>
2023-07-17 15:22:18 +0900

    current_userからのデータ取得へ変更

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -1,25 +1,10 @@
 class LendingsController < ApplicationController
-  def index
-    @books = Book.all
-    @lendings = Lending.all
-
-    session[:user] = { "id" => 1 } 
-
-    if session[:user] && session[:user]["id"]
-      lendingbooks = Book.joins(:lendings).where(lendings: { user_id: session[:user]["id"] }).where("lendings.start_date <= ? AND lendings.end_date >= ?", Date.today, Date.today)
-    else
-      lendingbooks = []
-    end
-
-    @orderlendingbooks = lendingbooks.order("lendings.end_date DESC")
-  end
-
   def create
-    Lending.create(lending_params)
-    redirect_to books_path
+    current_user.lendings.create(lending_params)
+    redirect_to book_path(Book.find(lending_params[:book_id]))
   end
 
   def lending_params
-    params.require(:lending).permit(:start_date, :end_date, :book_id, :user_id)
+    params.require(:lending).permit(:start_date, :end_date, :book_id)
   end
 end
