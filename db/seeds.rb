9f567527a7ee10e115d18171d8b23912dacd4eb0
touyama <yusuke2710@icloud.com>
2023-07-17 11:17:23 +0900

    index seeds変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-start_date_range = Date.new(2023, 6, 1)..Date.new(2023, 8, 15)
-3.times{|n| User.create!(name: "test#{n}")}
+3.times{|n| User.create!(mail: "test#{n}",name: "test#{n}",password: "test#{n}")}
 3.times{|n| Book.create!(title: "book#{n}", image_name: "noImage.jpg")}
 
-start_date = start_date_range.to_a.sample
-end_date = start_date + 7
+Lending.create!(user_id: "1",book_id: "1",start_date: "Mon, 17 Jul 2023",end_date:"Tue, 25 Jul 2023")
+Lending.create!(user_id: "2",book_id: "2",start_date: "Mon, 17 Jul 2023",end_date:"Tue, 25 Jul 2023")
 
-User.all.each do |user|
-  Book.all.each do |book|
-    user.lendings.create!(book: book, start_date: start_date, end_date: end_date)
-  end
-end
+
+
