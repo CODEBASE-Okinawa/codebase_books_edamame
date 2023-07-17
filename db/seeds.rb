5287876a35ca0702853838509293910dccffd5ae
touyama <yusuke2710@icloud.com>
2023-07-17 17:18:02 +0900

    seed更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-4.times{|n| User.create!(mail: "test#{n}",name: "test#{n}",password: "test#{n}")}
+5.times{|n| User.create!(mail: "test#{n}",name: "test#{n}",password: "test#{n}")}
 3.times{|n| Book.create!(title: "book#{n}", image_name: "noImage.jpg")}
 
 Lending.create!(user_id: "1",book_id: "1",start_date: "Mon, 17 Jul 2023",end_date:"Tue, 25 Jul 2023")
 Lending.create!(user_id: "2",book_id: "2",start_date: "Mon, 17 Jul 2023",end_date:"Tue, 25 Jul 2023")
-Lending.create!(user_id: "4",book_id: "3",start_date: "Mon, 17 Jul 2023",end_date:"Tue, 25 Jul 2023")
+Lending.create!(user_id: "5",book_id: "3",start_date: "Mon, 17 Jul 2023",end_date:"Tue, 25 Jul 2023")
