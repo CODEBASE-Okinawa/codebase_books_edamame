9813d28ee8e2f44dff3f12828a08f95a78e93180
koikeasuka <akoike@big-gate.co.jp>
2023-07-17 17:48:46 +0900

    軽微な修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find_by(id: params[:id])
-    @lend = Lending.where(book_id: params[:id])
+    @lend = Lending.where(book_id: params[:id]).find_by('end_date > ?', Date.today)
+    # binding.irb
   end
 end
