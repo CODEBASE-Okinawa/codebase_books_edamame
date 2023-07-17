a0bdd3d57cc1c9e4a17515b423df559423ae6d5b
touyama <yusuke2710@icloud.com>
2023-07-17 16:28:12 +0900

    借りている一覧更新

--- app/controllers/lendings_controller.rb
+++ app/controllers/lendings_controller.rb
@@ -2,13 +2,12 @@ class LendingsController < ApplicationController
   def index
     @books = Book.all
     @lendings = Lending.all
-
-    if session[:user] && session[:user]["id"]
-      lendingbooks = Book.joins(:lendings).where(lendings: { user_id: session[:user]["id"] }).where("lendings.start_date <= ? AND lendings.end_date >= ?", Date.today, Date.today)
+    if session[:user_id]
+      lendingbooks = Book.joins(:lendings).where(lendings: { user: current_user }).where("lendings.start_date <= ? AND lendings.end_date >= ?", Date.today, Date.today)
     else
       lendingbooks = []
     end
-    if lendingbooks.nil?
+    if !lendingbooks.nil?
       @orderlendingbooks = lendingbooks.order("lendings.end_date ASC")
     end
   end
