3bbe5c3ee233d4f165df6e868d3130520de9403a
HY <givhvyvuc@gmail.com>
2023-08-06 00:41:02 +0900

    サインイン、サインアップ、本一覧表示を修正しました

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,13 @@
 module BooksHelper
+  def lending_status(book, current_user)
+    lending = Lending.where('start_date <= ?', Date.today)
+    .where('end_date >= ?', Date.today).find_by(book: book)
+    
+    if lending && current_user
+      return '現在借りています'
+    elsif lending
+      return '貸し出し中'
+    else return '貸出可能'
+    end
+  end
 end
